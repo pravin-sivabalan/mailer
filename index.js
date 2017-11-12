178eb3abf372e5b2382651ee1d1f523d66956f06
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-11-12 15:20:17 -0500

    add formating stuff

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const nodemailer = require('nodemailer');
-const config = require('./config.json');
+const fs = require('fs');
 const csv = require('csvtojson');
+const config = require('./config.json');
+
+const csvFilePath = process.argv[2];
 
 let transporter = nodemailer.createTransport(config);
 
-csv().fromFile(csvFilePath).on('json', 'utf8', (person) => {
-  fs.readFile('template.html', (err, content) => {
+csv().fromFile(csvFilePath).on('json', (person) => {
+  fs.readFile('template.html', 'utf8', (err, content) => {
+
+    const link = 'http://interview.boilermake.org/invite' + person.url.substring(person.url.lastIndexOf('/'));
+    content = content.replace('#{name}', person.first);
+    content = content.replace('#{link}', link);
+    content = content.replace('#{link}', link);
+
     let mailOptions = {
       from: '"BoilerMake Team" <psivabal@purdue.edu>',
-      to: person.first,
+      to: person.email,
       subject: 'BoilerMake Exec Team Interview',
       html: content
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
       if(error) return console.log(error);
-      console.log('Message sent: ' + info.response);
+      console.log('Message sent to: ' + person.first + ' ' + person.last);
     });
+
+  });
 })
-.on('done',(error)=>{
+.on('done', (error) => {
     if(error) console.log(error);
 });
